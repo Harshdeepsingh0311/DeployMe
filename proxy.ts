9903f27a6adaeca2b2a88e16402c40128da7061f
Harshdeep Singh <harshdeepsinghbakshi2005@gmail.com>
2026-01-15 00:37:10 +0530

    forgot-pwd 7

--- proxy.ts
+++ proxy.ts
@@ -3,50 +3,51 @@ import type { NextRequest } from "next/server"
 import { createServerClient } from "@supabase/ssr"
 
 export function proxy(req: NextRequest) {
-  // ✅ Allow API routes to pass through untouched
-  if (
-    req.nextUrl.pathname.startsWith("/auth/reset-password") ||
-    req.nextUrl.pathname.startsWith("/auth/forget-password")
-  ) {
-    return NextResponse.next()
-  }
-  if (req.nextUrl.pathname.startsWith("/api")) {
-    return NextResponse.next()
-  }
+  // // ✅ Allow API routes to pass through untouched
+  // if (
+  //   req.nextUrl.pathname.startsWith("/auth/reset-password") ||
+  //   req.nextUrl.pathname.startsWith("/auth/forget-password")
+  // ) {
+  //   return NextResponse.next()
+  // }
+  // if (req.nextUrl.pathname.startsWith("/api")) {
+  //   return NextResponse.next()
+  // }
 
-  const res = NextResponse.next()
+  // const res = NextResponse.next()
 
-  const supabase = createServerClient(
-    process.env.NEXT_PUBLIC_SUPABASE_URL!,
-    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
-    {
-      cookies: {
-        get(name: string) {
-          return req.cookies.get(name)?.value
-        },
-        set(name: string, value: string, options: any) {
-          res.cookies.set({ name, value, ...options })
-        },
-        remove(name: string, options: any) {
-          res.cookies.set({ name, value: "", ...options })
-        },
-      },
-    }
-  )
+  // const supabase = createServerClient(
+  //   process.env.NEXT_PUBLIC_SUPABASE_URL!,
+  //   process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
+  //   {
+  //     cookies: {
+  //       get(name: string) {
+  //         return req.cookies.get(name)?.value
+  //       },
+  //       set(name: string, value: string, options: any) {
+  //         res.cookies.set({ name, value, ...options })
+  //       },
+  //       remove(name: string, options: any) {
+  //         res.cookies.set({ name, value: "", ...options })
+  //       },
+  //     },
+  //   }
+  // )
 
-  return supabase.auth.getSession().then(({ data: { session } }) => {
-    const pathname = req.nextUrl.pathname
+  // return supabase.auth.getSession().then(({ data: { session } }) => {
+  //   const pathname = req.nextUrl.pathname
 
-    if (!session && pathname.startsWith("/dashboard")) {
-      return NextResponse.redirect(new URL("/auth/login", req.url))
-    }
+  //   if (!session && pathname.startsWith("/dashboard")) {
+  //     return NextResponse.redirect(new URL("/auth/login", req.url))
+  //   }
 
-    if (session && pathname.startsWith("/auth")) {
-      return NextResponse.redirect(new URL("/dashboard", req.url))
-    }
+  //   if (session && pathname.startsWith("/auth")) {
+  //     return NextResponse.redirect(new URL("/dashboard", req.url))
+  //   }
 
-    return res
-  })
+  //   return res
+  // })
+  return NextResponse.next();
 }
 
 /**

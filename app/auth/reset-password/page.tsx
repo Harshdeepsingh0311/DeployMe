38f9d6865fd6ad09cd138002c197c963c55d3987
Harshdeep Singh <harshdeepsinghbakshi2005@gmail.com>
2026-01-14 23:57:48 +0530

    forgot-pwd 1

--- app/auth/reset-password/page.tsx
+++ app/auth/reset-password/page.tsx
@@ -1,5 +1,6 @@
-export const dynamic = "force-dynamic"
 "use client"
+
+export const dynamic = "force-dynamic"
 
 import { supabase } from "@/utils/supabase/client"
 import { useEffect, useState } from "react"
